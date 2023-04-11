dc7dd5f23b4a997d7ac1a220a9a0fe250e6bb77d
LuisPulenta <luisalbertonu@gmail.com>
2023-04-11 18:38:05 -0300

    frmLogin

--- WIN/frmLogin.Designer.cs
+++ WIN/frmLogin.Designer.cs
@@ -3,12 +3,12 @@
     partial class frmLogin
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,17 +23,134 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            txtUsuario = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            txtClave = new TextBox();
+            btnAceptar = new Button();
+            btnCancelar = new Button();
+            pictureBox1 = new PictureBox();
+            errorProvider1 = new ErrorProvider(components);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).BeginInit();
+            SuspendLayout();
+            // 
+            // txtUsuario
+            // 
+            txtUsuario.Location = new Point(111, 34);
+            txtUsuario.Name = "txtUsuario";
+            txtUsuario.Size = new Size(116, 27);
+            txtUsuario.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(29, 34);
+            label1.Name = "label1";
+            label1.Size = new Size(62, 20);
+            label1.TabIndex = 1;
+            label1.Text = "&Usuario:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(5, 73);
+            label2.Name = "label2";
+            label2.Size = new Size(86, 20);
+            label2.TabIndex = 2;
+            label2.Text = "&Contraseña:";
+            // 
+            // txtClave
+            // 
+            txtClave.Location = new Point(111, 73);
+            txtClave.Name = "txtClave";
+            txtClave.PasswordChar = '*';
+            txtClave.Size = new Size(116, 27);
+            txtClave.TabIndex = 3;
+            // 
+            // btnAceptar
+            // 
+            btnAceptar.Image = (Image)resources.GetObject("btnAceptar.Image");
+            btnAceptar.ImageAlign = ContentAlignment.TopCenter;
+            btnAceptar.Location = new Point(19, 144);
+            btnAceptar.Name = "btnAceptar";
+            btnAceptar.Size = new Size(94, 57);
+            btnAceptar.TabIndex = 4;
+            btnAceptar.Text = "&Aceptar";
+            btnAceptar.TextAlign = ContentAlignment.BottomCenter;
+            btnAceptar.UseVisualStyleBackColor = true;
+            btnAceptar.Click += btnAceptar_Click_1;
+            // 
+            // btnCancelar
+            // 
+            btnCancelar.Image = (Image)resources.GetObject("btnCancelar.Image");
+            btnCancelar.ImageAlign = ContentAlignment.TopCenter;
+            btnCancelar.Location = new Point(133, 144);
+            btnCancelar.Name = "btnCancelar";
+            btnCancelar.Size = new Size(94, 57);
+            btnCancelar.TabIndex = 5;
+            btnCancelar.Text = "Ca&ncelar";
+            btnCancelar.TextAlign = ContentAlignment.BottomCenter;
+            btnCancelar.UseVisualStyleBackColor = true;
+            btnCancelar.Click += btnCancelar_Click_1;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(275, 34);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(125, 127);
+            pictureBox1.TabIndex = 6;
+            pictureBox1.TabStop = false;
+            // 
+            // errorProvider1
+            // 
+            errorProvider1.ContainerControl = this;
+            // 
+            // frmLogin
+            // 
+            AcceptButton = btnAceptar;
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.SkyBlue;
+            CancelButton = btnCancelar;
+            ClientSize = new Size(427, 233);
+            Controls.Add(pictureBox1);
+            Controls.Add(btnCancelar);
+            Controls.Add(btnAceptar);
+            Controls.Add(txtClave);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtUsuario);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "frmLogin";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "--- Ingreso al Sistema ---";
+            Load += frmLogin_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtUsuario;
+        private Label label1;
+        private Label label2;
+        private TextBox txtClave;
+        private Button btnAceptar;
+        private Button btnCancelar;
+        private PictureBox pictureBox1;
+        private ErrorProvider errorProvider1;
     }
 }
--- WIN/frmLogin.cs
+++ WIN/frmLogin.cs
@@ -1,3 +1,6 @@
+﻿using System;
+using System.Windows.Forms;
+
 namespace WIN
 {
     public partial class frmLogin : Form
@@ -5,6 +8,36 @@ namespace WIN
         public frmLogin()
         {
             InitializeComponent();
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+        private void btnAceptar_Click_1(object sender, EventArgs e)
+        {
+            errorProvider1.Clear();
+            if (txtUsuario.Text == string.Empty)
+            {
+                errorProvider1.SetError(txtUsuario, "Debe ingresar un Usuario");
+                txtUsuario.Focus();
+                return;
+            }
+            if (txtClave.Text == string.Empty)
+            {
+                errorProvider1.SetError(txtClave, "Debe ingresar una Contraseña");
+                txtClave.Focus();
+                return;
+            }
+        }
+
+        private void btnCancelar_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
